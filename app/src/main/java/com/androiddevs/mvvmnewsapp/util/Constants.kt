e71e6213e58736f535174b5fe46d482c381e39b2
nicholcr <nicholcr@umich.edu>
2022-06-21 22:22:58 -0400

    set up retrofit

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "ce7fa76285784ad28c71294a55727f2c"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
