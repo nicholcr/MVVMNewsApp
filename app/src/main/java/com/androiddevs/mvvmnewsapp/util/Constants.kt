670d9c51a3c4f4d5d92f4054e93c486204e0c0ee
nicholcr <nicholcr@umich.edu>
2022-08-07 17:17:16 -0400

    Added search functionality

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "ce7fa76285784ad28c71294a55727f2c"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
