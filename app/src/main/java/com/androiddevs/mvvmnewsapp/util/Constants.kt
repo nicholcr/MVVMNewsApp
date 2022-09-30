3ae242f15956c69305eb32ad38a3669c67bfda6b
nicholcr <nicholcr@umich.edu>
2022-09-30 19:41:54 -0400

    Added pagination to breakingNewsFragment and searchNewsFragment

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "ce7fa76285784ad28c71294a55727f2c"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
